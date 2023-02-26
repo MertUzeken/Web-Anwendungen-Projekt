10f8755047805e916af31edbb690eec520a257c8
BAryan96 <33257132+BAryan96@users.noreply.github.com>
2023-02-26 20:05:59 +0100

    first
    first

--- Musictool/src/main/java/edu/fra/uas/controller/project/ProjectController.java
+++ Musictool/src/main/java/edu/fra/uas/controller/project/ProjectController.java
@@ -1,7 +1,7 @@
 package edu.fra.uas.controller.project;
 
-import edu.fra.uas.service.project.*;
-import org.springframework.beans.factory.annotation.Autowired;
+//import edu.fra.uas.service.project.*;
+//#import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,16 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ProjectController {
 
-@Autowired
-private ProjectService ProjectService;
+//@Autowired
+//private ProjectService ProjectService;
 
     @GetMapping("/project")
         public String loadProject(){
             return "project";
     }
-
-    
-
-
 }
     
