4b62f9d720c5e5d5d32a1420f5dbfea0a0941c42
MertUzeken <mertuzeken@gmail.com>
2023-03-05 01:51:23 +0100

    Revert "JSON In+output"
    This reverts commit 335728ec88858b949e3d37bf0bc7794a9a670768.

--- Musictool/src/main/java/edu/fra/uas/service/project/ProjectServiceImpl.java
+++ Musictool/src/main/java/edu/fra/uas/service/project/ProjectServiceImpl.java
@@ -1,19 +1,15 @@
 package edu.fra.uas.service.project;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import edu.fra.uas.model.project.Project;
 import edu.fra.uas.repository.project.ProjectRepository;
 
 @Component
-@Service
 public class ProjectServiceImpl implements ProjectService {
-    public List<Project> localStore = new ArrayList<>();
 
 	@Autowired
 	private ProjectRepository projectRepository;
@@ -46,31 +42,16 @@ public class ProjectServiceImpl implements ProjectService {
 	@Override
 	public void saveProject(Project project) {
 		this.projectRepository.save(project);
+
 	}
 
     @Override
     public List<Project> showAllProject() {
         // TODO Auto-generated method stub
-        /*  for (int counter = 0; counter < localStore.size(); counter++){
-            System.out.println(localStore.get(counter).getProjectName());
-        } */
         throw new UnsupportedOperationException("Unimplemented method 'showAllProject'");
     }
 
 
-   
-    public void saveLocal(Project project){
-        localStore.add(project);
-    }
+    
 
-    public Project getLocalProjByName(String name){
-
-        for (Project project : localStore){
-            if(project.getProjectName().equals(name)){
-                return project;
-            }
-        }
-        return null;
-        
-    }
 }
